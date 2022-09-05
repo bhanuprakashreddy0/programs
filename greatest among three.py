62ca80898944a54b816591ccaed2de7b324c949a
bhanuprakashreddy0 <112851066+bhanuprakashreddy0@users.noreply.github.com>
2022-09-05 14:17:07 +0530

    Add files via upload

---
+++ greatest among three.py
@@ -0,0 +1,13 @@
+a=input("enter the a value:")
+b=input("enter the b value:")
+c=input("enter the c value:")
+if (a>b):
+    if(a>c):
+        print("the gratest number is:",a)
+    else:
+        print("the greatest number is:",c)
+else:
+    if(b>c):
+        print("the greaest number is:",b)
+    else:
+        print("the greatest number is:",c)
