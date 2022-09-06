d4a26a10d884154fb4f5f413e21e7cbe310cdcd2
bhanuprakashreddy0 <112851066+bhanuprakashreddy0@users.noreply.github.com>
2022-09-06 14:01:51 +0530

    Add files via upload

---
+++ count of charaters.py
@@ -0,0 +1,6 @@
+string=input("enter the sentence:")
+print("the original string is:" + string)
+
+res=len(string.split())
+print("the no of words in string are:" + str(res))
+print("the nof characters:",len(string))
