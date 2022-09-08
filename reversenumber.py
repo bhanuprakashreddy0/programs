2a7fb8fbf228d0fcfd973acc3b50183ae2004787
bhanuprakashreddy0 <112851066+bhanuprakashreddy0@users.noreply.github.com>
2022-09-08 12:33:25 +0530

    Add files via upload

--- reversenumber.py
+++ reversenumber.py
@@ -1,7 +1,12 @@
-number=int(input("enter the number:"))
-reversenumber=0
-while number>0:
-    remainder=number%10
-    reversenumber=reversenumber*10+remainder
-    number=number//10
-print(reversenumber)
+try:
+    number=int(input("enter the number:"))
+    reversenumber=0
+    while number>0:
+        remainder=number%10
+        reversenumber=reversenumber*10+remainder
+        number=number//10
+    print(reversenumber)
+except ValueError:
+    print("enter the input as integers only")
+    
+    
