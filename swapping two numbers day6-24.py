135d276eee4efcf475b62c26611ca07de8eb0dc0
bhanuprakashreddy0 <112851066+bhanuprakashreddy0@users.noreply.github.com>
2022-10-10 13:44:16 +0530

    Add files via upload

---
+++ swapping two numbers day6-24.py
@@ -0,0 +1,8 @@
+x=int(input("enter the value of x:"))
+y=int(input('enter the value of y:'))
+temp=x
+x=y
+y=temp
+print("the value of x after swapping:",x)
+print("the value of y after swapping:",y)
+            
