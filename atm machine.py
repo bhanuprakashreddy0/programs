13603b829b030432d0ccc512a65beb19b6a1f7ff
bhanuprakashreddy0 <112851066+bhanuprakashreddy0@users.noreply.github.com>
2022-11-08 11:23:33 +0530

    Add files via upload

---
+++ atm machine.py
@@ -0,0 +1,10 @@
+d1=int(input("enter the denomination:"))
+n1=int(input("enter the no of notes of d1:"))
+d2=int(input("enter the denomination:"))
+n2=int(input("enter the no of notes of d2:"))
+d3=int(input("enter the denomination:"))
+n3=int(input("enter the no of notes of d3:"))
+total=d1*n1+d2*n2+d3*n3
+print("the total amount in atm:",total)
+
+
